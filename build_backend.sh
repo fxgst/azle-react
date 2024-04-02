fa2f0cb11e5c062f7793748eba7d16b1eefc412e
Elias Datler <elias.datler@dfinity.org>
2024-04-02 14:38:04 +0900

    Fix deploy to mainnet

---
+++ build_backend.sh
@@ -0,0 +1,6 @@
+source .env
+if [ "$DFX_NETWORK" == "local" ] 
+    then VITE_CANISTER_URL=http://$(dfx canister id backend).localhost:4943 npm run build --workspaces --if-present
+elif [ "$DFX_NETWORK" == "ic" ]
+    then VITE_CANISTER_URL=https://$(dfx canister id backend --ic).raw.icp0.io npm run build --workspaces --if-present
+fi
