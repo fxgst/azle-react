8b586b0969c9938bb04fc3c5d412c32fc0b1de61
Elias Datler <elias.datler@dfinity.org>
2024-04-07 22:28:38 +0800

    Add more endpoints

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -3,9 +3,35 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.get('/greet', async (req, res) => {
+let phonebook = {
+    'Alice': { 'phone': '123-456-789', 'added': new Date() }
+};
+
+app.get('/contacts', (_req, res) => {
+    res.json(phonebook);
+});
+
+app.post('/contacts/add', (req, res) => {
+    if (Object.keys(phonebook).includes(req.body.name)) {
+        res.json({ error: 'Name already exists' });
+    } else {
+        const contact = { [req.body.name]: { phone: req.body.phone, added: new Date() } };
+        phonebook = { ...phonebook, ...contact };
+        res.json({ status: 'Ok' });
+    }
+});
+
+app.get('/greet', (req, res) => {
     res.json({ greeting: `Hello, ${req.query.name}` });
 });
 
 app.use(express.static('/dist'));
+
+app.post('/price-oracle', async (req, res) => {
+    const pair = req.body.pair;
+    const response = await fetch(`https://api.binance.com/api/v3/avgPrice?symbol=${pair}`);
+    const json = await response.json();
+    res.json(json);
+});
+
 app.listen();
