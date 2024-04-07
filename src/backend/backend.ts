724f31c524de4a6f19e81cfdced15304b6476259
Elias Datler <elias.datler@dfinity.org>
2024-04-07 15:15:39 +0000

    Remove code

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -3,35 +3,6 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-let phonebook = {
-    'Alice': { 'phone': '123-456-789', 'added': new Date() }
-};
-
-app.get('/contacts', (_req, res) => {
-    res.json(phonebook);
-});
-
-app.post('/contacts/add', (req, res) => {
-    if (Object.keys(phonebook).includes(req.body.name)) {
-        res.json({ error: 'Name already exists' });
-    } else {
-        const contact = { [req.body.name]: { phone: req.body.phone, added: new Date() } };
-        phonebook = { ...phonebook, ...contact };
-        res.json({ status: 'Ok' });
-    }
-});
-
-app.get('/greet', (req, res) => {
-    res.json({ greeting: `Hello, ${req.query.name}` });
-});
-
-app.use(express.static('/dist'));
-
-app.post('/price-oracle', async (req, res) => {
-    const pair = req.query.pair;
-    const response = await fetch(`https://api.exchange.coinbase.com/products/${pair}/ticker`);
-    const json = await response.json();
-    res.json(json);
-});
+// TODO: add endpoints
 
 app.listen();
